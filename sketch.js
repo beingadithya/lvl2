623b89e2b550f1342338a4572337a0065911910d
theInevtable007 <beingadithya06@gmail.com>
2021-02-13 14:52:23 +0530

    Adithya

--- sketch.js
+++ sketch.js
@@ -675,7 +675,7 @@ var fish = createSprite(random(1830,1700),random(100,450));
 }
 
 function greet() {
-  window.location.href = 'https://theinevitable007.github.io/3/';
+  window.location.href = 'https://beingadithya.github.io/3/';
 }
 
 function retry() {
